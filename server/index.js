4241dbc6716f51e01b4d90631788214c92982a61
Maxim A <m_altuhov@mail.ru>
2022-09-19 20:50:05 +0300

    Создал модели для БД, перв. настройка

--- server/index.js
+++ server/index.js
@@ -1,6 +1,23 @@
 require('dotenv').config();
 const express = require('express');
-const app = express();
+const sequelize = require('./db');
+const models = require('./models/models');
+const cors = require('cors');
+
 const PORT = process.env.PORT || 5000;
 
-app.listen(PORT, () => console.log(`start port ${PORT}`));
+const app = express();
+app.use(cors());
+app.use(express.json());
+
+const start = async () => {
+  try {
+    await sequelize.authenticate();
+    await sequelize.sync();
+    app.listen(PORT, () => console.log(`start port ${PORT}`));
+  } catch (error) {
+    console.log(error);
+  }
+};
+
+start();
